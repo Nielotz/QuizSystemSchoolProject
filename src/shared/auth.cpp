1fd01c4135a79491d1f5485fbe7fa0ecb06eb91b
Nielotz <mati2806@poczta.fm>
2022-01-14 18:35:46 +0100

    Fix typo

--- src/shared/auth.cpp
+++ src/shared/auth.cpp
@@ -5,7 +5,7 @@ Auth::Auth()
 
 }
       
-UserData attemp_login(Control::Control control)
+UserData attempt_login(Control::Control control)
 {
    UserData ud("", "", AccountType::kStudent);
    return ud;
