6bb6e12e9c7f2aff89754f024a35fbf7a894383b
Nielotz <mati2806@poczta.fm>
2022-01-14 20:19:56 +0100

    Change attempt_login to login, add function body

--- src/shared/auth.cpp
+++ src/shared/auth.cpp
@@ -1,12 +1,20 @@
+#include <optional>
+
 #include "../headers/shared/auth.h"
+#include "../headers/control/auth.h"
+#include "../headers/database/user.h"
 
-Auth::Auth()
+UserData Auth::login()
 {
+    Database::User user_database;
+    while (true)
+    {
+        auto received_user_credentials = Control::Auth::get_auth_data();
+        const string &username = received_user_credentials.first;
+        const string &password = received_user_credentials.second;
 
-}
-      
-UserData attempt_login(Control::Control control)
-{
-   UserData ud("", "", AccountType::kStudent);
-   return ud;
+        optional <UserData> received_user_data = user_database.get_user(username, password);
+        if (received_user_data.has_value())
+            return *received_user_data;
+    }
 }
