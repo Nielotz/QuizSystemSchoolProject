f506854449f5b7302502ee98e05170d0b309ac41
Nielotz <mati2806@poczta.fm>
2022-01-16 19:29:58 +0100

    Join Answers, Question and TestData in one file, add TestData constructor

--- src/headers/test_data/test_data.h
+++ src/headers/test_data/test_data.h
@@ -5,23 +5,40 @@
 #include <utility>
 #include <string>
 
-#include "../test_data/question.h"
-#include "../test_data/answer.h"
 #include "../shared/user_data.h"
-#include "../test_data/question.h"
 
 using namespace std;
 
 namespace TestData
 {
-	struct TestData
-	{
-	public:
-		std::string name;
-		vector<Question> questions;
-		vector<pair<string, Answer>> students_answers;
-		vector<pair<string, string>> reported_issues;
-	};
+    struct Answers
+    {
+    public:
+        vector<string> answers_;
+
+        explicit Answers(vector<string> answers);
+    };
+
+    struct Question
+    {
+    public:
+        string question_;
+        Answers answers_;
+        vector<string> correct_answers_;
+
+        Question(string question, Answers answers, vector<string> correct_answers);
+    };
+
+    struct TestData
+    {
+    public:
+        std::string name;
+        vector<Question> questions;
+        vector<pair<string, Answers>> students_answers;
+        vector<pair<string, string>> reported_issues;
+
+        TestData(std::string name, vector<Question> questions);
+    };
 }
 
 #endif //SRC_TEST_DATA_TEST_DATA_H
--- src/test_data/test_data.cpp
+++ src/test_data/test_data.cpp
@@ -1,5 +1,17 @@
 #include "../headers/test_data/test_data.h"
 
+#include <utility>
+
 using namespace std;
 
+
+TestData::Answers::Answers(vector<string> answers) : answers_(std::move(answers))
+{}
+
+TestData::Question::Question(string question, Answers answers, vector<string> correct_answers)
+        : question_(std::move(question)), answers_(std::move(answers)), correct_answers_(std::move(correct_answers))
+{}
+
+TestData::TestData::TestData(std::string name, vector<Question> questions) : name(std::move(name)), questions(std::move(questions))
+{}
 
