e053edd1c00409f25003723c09e38006f1cb3e3e
Nielotz <mati2806@poczta.fm>
2022-01-16 19:28:12 +0100

    Add Control::TestMenu::Teacher, Control::TestMenu::Student

--- src/headers/control/test_menu/test_menu.h
+++ src/headers/control/test_menu/test_menu.h
@@ -4,16 +4,26 @@
 #include "..\test_menu\option_type.h"
 #include "../../test_data/test_data.h"
 
-namespace Control
+namespace Control::TestMenu
 {
-    namespace TestMenu
+    class TestMenu
     {
-        class TestMenu
-        {
-        public:
-            OptionType control_test_menu_selection(TestData::TestData test_data);
-        };
-    }
+    public:
+        virtual OptionType control_test_menu_selection(TestData::TestData test_data) = 0;
+    };
+
+    class Teacher : public TestMenu
+    {
+    public:
+        OptionType control_test_menu_selection(TestData::TestData test_data) override;
+    };
+
+    class Student : public TestMenu
+    {
+    public:
+        OptionType control_test_menu_selection(TestData::TestData test_data) override;
+    };
+
 }
 
 #endif //SRC_CONTROL_TEST_MENU_TEST_MENU_H
