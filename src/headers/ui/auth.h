6663849989b10ae298eb21ecb5c8063fe2f44890
Ulania <janiszkiewic@student.agh.edu.pl>
2022-01-16 19:37:28 +0100

    ui auth

--- src/headers/ui/auth.h
+++ src/headers/ui/auth.h
@@ -1,13 +1,17 @@
 #ifndef SRC_AUTH_H
 #define SRC_AUTH_H
 
+#include <string>
+
+using namespace std;
+
 namespace UI
 {
     class Auth
     {
     public:
-        void ask_username();
-        void ask_password();
+        static void ask_username(bool is_retry);
+        static void ask_password(string username, bool is_retry);
     };
 }
 #endif //SRC_AUTH_H
--- src/ui/auth.cpp
+++ src/ui/auth.cpp
@@ -1,15 +1,90 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <conio.h>
+
 #include "../headers/ui/auth.h"
 
 namespace UI
 {
-   void Auth::ask_username()
+   static void Auth::ask_username(bool is_retry)
     {
+       char c;
+       string input;
+       const char ENTER = 13;
 
+       cout << "Quiz system" << endl;
+       cout << "Logging in" << endl;
+       cout << "username: ";
+
+       do
+       {
+           c = _getch();
+
+           if (isprint(c))
+           {
+               input += c;
+               cout << c;
+           }
+
+           else if (c == 8 && !input.empty())
+           {
+               input.pop_back();
+               cout << "\b \b";
+           }
+       } 
+       while (c != ENTER);
+       system("CLS");
+
+       UI::Auth::ask_password(input, is_retry);
     }
 
-    void Auth::ask_password()
+    static void Auth::ask_password(string username, bool is_retry)
     {
+        char c;
+        string input;
+        const char ENTER = 13;
 
+        cout << "Quiz system" << endl;
+        cout << "Logging in" << endl;
+        cout << "username: " << username << endl;
+        cout << "password: ";
+
+        do
+        {
+            c = _getch();
+            if (isprint(c))
+            {
+                input += c;
+                cout << "*";
+            }
+
+            else if (c == 8 && !input.empty())
+            {
+                input.pop_back();
+                cout << "\b \b";
+            }
+        } while (c != ENTER);
+
+       // if (username == username_ && input == password_)
+       // {
+       //     is_retry = true;
+       // }
+
+        system("CLS");
+
+        if (is_retry == false)
+        {
+            cout << "Quiz system" << endl;
+            cout << "Logging in" << endl << endl;
+            cout << "Invalid auth data." << endl << endl;
+            cout << "press any key to try again" << endl;
+            char c;
+            c = _getch();
+
+            system("CLS");
+            UI::Auth::ask_username(false);
+        }
     }
 }
 
