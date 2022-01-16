30d1379d05f161c58a7809a3415fcbb079c54a07
Nielotz <mati2806@poczta.fm>
2022-01-16 19:25:34 +0100

    Add init test_selector_

--- src/control/control.cpp
+++ src/control/control.cpp
@@ -4,6 +4,12 @@ namespace Control
 {
     Control::Control(AccountType account_type)
     {
+        if (account_type == AccountType::kStudent){
+            test_selector_ = make_unique<TestSelector::Student>();
+        }
+        else{
+            test_selector_ = make_unique<TestSelector::Teacher>();
+        }
     }
 }
 
