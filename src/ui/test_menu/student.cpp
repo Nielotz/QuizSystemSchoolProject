55e304eda10ab5d393cadde5c6dfd5901addafc1
Ulania <janiszkiewic@student.agh.edu.pl>
2022-01-19 18:05:42 +0100

    Add TestMenuUI

--- src/ui/test_menu/student.cpp
+++ src/ui/test_menu/student.cpp
@@ -1,7 +1,30 @@
-#include "../../headers/ui/test_menu/student.h"
+#include <iostream>
 
-void ui::test_menu::Student::show(string test_name, string username, size_t amount_of_questions, unordered_map<string, int> users_points,
-                                  string selected_answer)
+#include "../../headers/ui/test_menu/student.h"
+#include "../../headers/ui/ui.h"
+#include "../../headers/test_data/test_data.h"
+
+void ui::test_menu::Student::show(std::string test_name, std::string username, size_t amount_of_questions, unordered_map<std::string, int> users_points,
+	std::string selected_answer)
 {
-    // TODO: Show student test menu.
+	system("CLS");
+
+	ui::UI::print_logo();
+
+	std::cout << "  =============" << std::endl;
+	std::cout << "  + Test Menu +" << std::endl;
+	std::cout << "  =============" << std::endl << std::endl;
+
+	std::cout << "  Test: <" << test_name << ">" << std::endl;
+	std::cout << "      Number of questions: " << amount_of_questions << std::endl << std::endl;
+
+	std::cout << "  Completed test:" << std::endl;
+	for (auto& [username_, points] : users_points)
+	{
+		if(username_== username)
+		std::cout << "      " << username_ << " <" << points << "> / < " << amount_of_questions << std::endl;
+	}
+
+	std::cout << std::endl;
+	std::cout << "s - show, q - quit";
 }
--- src/ui/test_menu/teacher.cpp
+++ src/ui/test_menu/teacher.cpp
@@ -1,7 +1,36 @@
-#include "../../headers/ui/test_menu/teacher.h"
+#include <iostream>
 
-void ui::test_menu::Teacher::show(string test_name, string username, size_t amount_of_questions, unordered_map<string, int> users_points,
-                                  string selected_answer)
+#include "../../headers/ui/test_menu/teacher.h"
+#include "../../headers/ui/ui.h"
+#include "../../headers/test_data/test_data.h"
+
+void ui::test_menu::Teacher::show(std::string test_name, std::string username, size_t amount_of_questions, unordered_map<string, int> users_points,
+	std::string selected_answer)
 {
-    // TODO: Show teacher test menu.
+	system("CLS");
+
+	ui::UI::print_logo();
+
+	std::cout << "  =============" << std::endl;
+	std::cout << "  + Test Menu +" << std::endl;
+	std::cout << "  =============" << std::endl << std::endl;
+
+	std::cout << "  Test: <" << test_name << ">" << std::endl;
+	std::cout << "      Number of questions: " << amount_of_questions << std::endl << std::endl;
+
+	std::cout << "  Completed tests:" << std::endl;
+	for (auto& [username, points] : users_points)
+	{
+		if (selected_answer == username)
+		{
+			std::cout << "      ";
+			ui::UI::text_background_color(0, 3);
+			std::cout << username;
+			ui::UI::color_reset();
+			std::cout << "   <" << points << "> / < " << amount_of_questions << std::endl;
+		}
+		std::cout << "      " << username << " <" << points << "> / < " << amount_of_questions << std::endl;
+	}
+
+	std::cout << "s - show, q - quit, e - edit, r - show students result, q - quit";
 }
