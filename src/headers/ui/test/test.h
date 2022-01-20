31607affabc34e5cbcb03e10c4daa5ed0c09adc4
Ulania <janiszkiewic@student.agh.edu.pl>
2022-01-20 23:52:31 +0100

    update test_ui

--- src/headers/ui/test/test.h
+++ src/headers/ui/test/test.h
@@ -6,18 +6,13 @@
 
 namespace ui::test
 {
-    using std::string;
-    using std::vector;
+    void show_edit_question(const std::string& test_name, const std::string& question, const std::vector<std::string>& answers,
+        const std::vector<std::string>& correct_answers, const std::string& selected_answer, int question_number, int question_amount);
 
-    void show_edit_question(const string &question, const vector<string> &answers, const vector<string> &correct_answers,
-                            const string &selected_answer, int question_number, int question_amount);
+    void show_review_question(const std::string& test_name, const std::string& question, const std::vector<std::string>& answers,
+        const std::vector<std::string>& correct_answers, const std::vector<std::string>& marked_answers, int question_number, int question_amount);
 
-    void show_review_question(const string &question, const vector<string> &answers, const vector<string> &correct_answers,
-                              const vector<string> &marked_answers, const string &selected_answer, int question_number,
-                              int question_amount);
-
-    void show_take_question(const string &question, const vector<string> &answers, const vector<string> &marked_answers,
-                            const string &selected_answer, int question_number, int question_amount);
+    void show_take_question(const std::string& test_name, const std::string& question, const std::vector<std::string>& answers, const std::vector<std::string>& marked_answers, int question_number, int question_amount);
 }
 
 #endif //SRC_UI_TEST_TEST_H
