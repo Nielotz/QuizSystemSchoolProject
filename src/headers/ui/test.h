670d5d85ca772fc983d8870facec63e7b5c88e56
Ulania <janiszkiewic@student.agh.edu.pl>
2022-01-20 15:40:06 +0100

    Add test ui

--- src/headers/ui/test.h
+++ src/headers/ui/test.h
@@ -4,4 +4,11 @@
 #include <string>
 #include <vector>
 
+namespace ui::test
+{
+	void show_edit_question(std::string question, std::vector<std::string> answers, std::vector<std::string> correct_answers, std::string selected_answer, int question_number, int question_amount);
+	void show_review_question(std::string question, std::vector<std::string> answers, std::vector<std::string> correct_answers, std::vector<std::string> marked_answers, std::string selected_answer, int question_number, int question_amount);
+	void show_take_question(std::string question, std::vector<std::string> answers, std::vector<std::string> marked_answers, std::string selected_answer, int question_number, int question_amount);
+}
+
 #endif //SRC_UI_TEST_TEST_H
