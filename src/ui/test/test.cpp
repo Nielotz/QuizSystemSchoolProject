761604f79226b2befc2bb67a79dd891942ea7243
Nielotz <mati2806@poczta.fm>
2022-01-20 21:11:59 +0100

    Revert "Add add_test ui, add confirmation ui, update teacher test_menu "

--- src/ui/test/test.cpp
+++ src/ui/test/test.cpp
@@ -1,114 +1,117 @@
 ﻿#include <iostream>
 
-#include "../headers/ui/test.h"
-#include "../headers/ui/ui.h"
+#include "../../headers/ui/test/test.h"
+#include "../../headers/ui/ui.h"
 
-void ui::test::show_edit_question(std::string question, std::vector<std::string> answers, std::vector<std::string> correct_answers, std::string selected_answer, int question_number, int question_amount)
+using std::cout;
+using std::wcout;
+using std::endl;
+
+namespace ui::test
 {
-    system("CLS");
-
-    ui::UI::print_logo();
-
-    std::cout << "  =================" << std::endl;
-    std::cout << "  + Test editing  +" << std::endl;
-    std::cout << "  =================" << std::endl << std::endl;
-
-    std::cout << "  Test: < " << test_data::TestData::name << " >" << std::endl << std::endl;
-    std::cout << "  Reported question:" << endl;
-    std::cout << "  Question [<" << question_number << ">/<" << question_amount << ">]:" << std::endl;
-    std::cout << "      <" << question << ">" << std::endl << std::endl;
-    std::cout << "  Answers" << std::endl;
-    std::cout << "      correct     answer" << endl;
-    for (auto answer_ : answers)
+    void show_edit_question(const string &question, const vector<string> &answers, const vector<string> &correct_answers,
+                            const string &selected_answer, int question_number, int question_amount)
     {
-        if (answer_ == marked_answer)
+        /*
+        system("CLS");
+
+        ui::UI::print_logo();
+
+        cout << "  =================" << endl;
+        cout << "  + Test editing  +" << endl;
+        cout << "  =================" << endl << endl;
+
+        cout << "  Test: < " << test_data::TestData::name << " >" << endl << endl;
+        cout << "  Reported question:" << endl;
+        cout << "  Question [<" << question_number << ">/<" << question_amount << ">]:" << endl;
+        cout << "      <" << question << ">" << endl << endl;
+        cout << "  Answers" << endl;
+        cout << "      correct     answer" << endl;
+        for (auto answer_: answers)
         {
-            cout << "      [X]    <" << answer_ << ">" << endl;
-        }
-        else
-        {
-            cout << "      [ ]    <" << answer_ << ">" << endl;
-        }
-    }
-    std::cout << "e - edit, enter, q - quit, a - add answer, d - delete answer";
-}
-
-void ui::test::show_review_question(std::string question, std::vector<std::string> answers, std::vector<std::string> correct_answers, std::vector<std::string> marked_answers, std::string selected_answer, int question_number, int question_amount)
-{
-    system("CLS");
-
-    ui::UI::print_logo();
-
-    std::cout << "  ==================" << std::endl;
-    std::cout << "  + Test Reviewing +" << std::endl;
-    std::cout << "  ==================" << std::endl << std::endl;
-
-    std::cout << "  Test: < " << test_data::TestData::name << " >" << std::endl << std::endl;
-    std::cout << "      Question: [<" << question_number << "> / <" << question_amount << ">]: " << question << std::endl;
-    std::cout << "      Correct:    Marked:     Answers:" << std::endl;
-
-    for (auto answer_ : answers)
-    {
-        if (answer_ == correct_answers)
-        {
-            cout << "    [X]";
-
             if (answer_ == marked_answer)
-                cout << "         [X] <" << answer_ << ">"<<std::endl;
+            {
+                cout << "      [X]    <" << answer_ << ">" << endl;
+            }
             else
-                cout << "         [ ] <" << answer_ << ">" << std::endl;
+            {
+                cout << "      [ ]    <" << answer_ << ">" << endl;
+            }
         }
-        else
-        {
-            cout << "    [ ] <" << answer_ << ">" << endl;
-
-            if (answer_ == marked_answer)
-                cout << "         [X] <" << answer_ << ">" << std::endl;
-            else
-                cout << "         [ ] <" << answer_ << ">" << std::endl;
-        }
+        cout << "e - edit, enter, q - quit, a - add answer, d - delete answer";
+         */
     }
-    std::wcout << u"r - report, → next, ← previous, q - quit";
-}
 
-void ui::test::show_take_question(std::string question, std::vector<std::string> answers, std::vector<std::string> marked_answers, std::string selected_answer, int question_number, int question_amount)
-{
-    system("CLS");
-
-    ui::UI::print_logo();
-
-    std::cout << "  ===============" << std::endl;
-    std::cout << "  + Taking Test +" << std::endl;
-    std::cout << "  ===============" << std::endl << std::endl;
-
-    std::cout << "  Test: <" << test_data::TestData::name << ">" << std::endl << std::endl;
-    std::cout << "  Question: [<" << question_number << "> / <" << question_amount << ">]: " << question << std::endl;
-    std::cout << "  Answers:" << std::endl;
-
-    for (auto answers_ : answers)
+    void show_review_question(const string &question, const vector<string> &answers, const vector<string> &correct_answers,
+                              const vector<string> &marked_answers, const string &selected_answer, int question_number, int question_amount)
     {
-        if (answer_ == selected_answer)
+        /*
+        system("CLS");
+
+        ui::UI::print_logo();
+
+        cout << "  ==================" << endl;
+        cout << "  + Test Reviewing +" << endl;
+        cout << "  ==================" << endl << endl;
+
+        cout << "  Test: < " << test_data::TestData::name << " >" << endl << endl;
+        cout << "      Question: [<" << question_number << "> / <" << question_amount << ">]: " << question << endl;
+        cout << "      Correct:    Marked:     Answers:" << endl;
+
+        for (const auto& answer_: answers)
         {
-            cout << "    [X] <" << answer << ">" << endl;
+            if (answer_ == correct_answers)
+            {
+                cout << "    [X]";
+
+                if (answer_ == marked_answer)
+                    cout << "         [X] <" << answer_ << ">" << endl;
+                else
+                    cout << "         [ ] <" << answer_ << ">" << endl;
+            }
+            else
+            {
+                cout << "    [ ] <" << answer_ << ">" << endl;
+
+                if (answer_ == marked_answer)
+                    cout << "         [X] <" << answer_ << ">" << endl;
+                else
+                    cout << "         [ ] <" << answer_ << ">" << endl;
+            }
         }
-        else
-        {
-            cout << "    [ ] <" << answer << ">" << endl;
-        }
+        wcout << u"r - report, → next, ← previous, q - quit";
+    */
     }
-    std::cout << "enter - mark/unmark answer, q - quit";
-}
 
-void add_test(std::string entered_name)
-{
-    system("CLS");
+    void
+    show_take_question(const string &question, const vector<string> &answers, const vector<string> &marked_answers,
+                       const string &selected_answer, int question_number, int question_amount)
+    {
+        /*
+        system("CLS");
 
-    ui::UI::print_logo();
+        ui::UI::print_logo();
 
-    std::cout << "  =============" << std::endl;
-    std::cout << "  + Add test +" << std::endl;
-    std::cout << "  =============" << std::endl << std::endl;
+        cout << "  ===============" << endl;
+        cout << "  + Taking Test +" << endl;
+        cout << "  ===============" << endl << endl;
 
-    cout << "     Name:" << entered_name;
+        cout << "  Test: <" << test_data::TestData::name << ">" << endl << endl;
+        cout << "  Question: [<" << question_number << "> / <" << question_amount << ">]: " << question << endl;
+        cout << "  Answers:" << endl;
 
+        for (const auto& answers_: answers)
+        {
+            if (answer_ == selected_answer)
+            {
+                cout << "    [X] <" << answer << ">" << endl;
+            }
+            else
+            {
+                cout << "    [ ] <" << answer << ">" << endl;
+            }
+        }
+        cout << "enter - mark/unmark answer, q - quit";
+         */
+    }
 }
