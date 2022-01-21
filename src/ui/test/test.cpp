052c6515e30cc42bfbef5d5c2df81bdb8fc582da
Ulania <janiszkiewic@student.agh.edu.pl>
2022-01-21 14:38:30 +0100

    Update test.cpp

--- src/ui/test/test.cpp
+++ src/ui/test/test.cpp
@@ -9,9 +9,9 @@ using std::endl;
 
 namespace ui::test
 {
-    void show_edit_question(const std::string &test_name, const std::string &question,
-                            const std::vector<std::string> &answers, const std::vector<std::string> &correct_answers,
-                            const std::string &selected_answer, int question_number, int question_amount)
+    void show_edit_question(const std::string& test_name, const std::string& question,
+        const std::vector<std::string>& answers, const std::vector<std::string>& correct_answers,
+        const std::string& selected_answer, int question_number, int question_amount)
     {
         system("CLS");
 
@@ -27,10 +27,10 @@ namespace ui::test
         std::cout << "      <" << question << ">" << std::endl << std::endl;
         std::cout << "  Answers" << std::endl;
         std::cout << "      correct    " << std::endl;
-        
+
         bool is_marked = false;
-        
-        for (const auto &answer_: answers)
+
+        for (const auto& answer_ : answers)
         {
             if (answer_ == selected_answer && is_marked == false)
             {
@@ -43,14 +43,14 @@ namespace ui::test
             else
                 std::cout << "     ";
 
-            for (auto &correct_answer: correct_answers)
+            for (auto& correct_answer : correct_answers)
             {
-                if (answer_ == correct_answer && is_marked==false)
+                if (answer_ == correct_answer && is_marked == false)
                 {
-                    std::cout << " [X]  "<< answer_ << std::endl;
+                    std::cout << " [X]  " << answer_ << std::endl;
                     is_marked = true;
 
-                } 
+                }
             }
 
             if (!is_marked)
@@ -60,9 +60,9 @@ namespace ui::test
         std::cout << "e - edit, enter, q - quit, a - add answer, d - delete answer";
     }
 
-    void show_review_question(const std::string &test_name, const std::string &question, const std::vector<std::string> &answers,
-                              const std::vector<std::string> &correct_answers, const std::vector<std::string> &marked_answers,
-                              int question_number, int question_amount)
+    void show_review_question(const std::string& test_name, const std::string& question, const std::vector<std::string>& answers,
+        const std::vector<std::string>& correct_answers, const std::vector<std::string>& marked_answers,
+        int question_number, int question_amount)
     {
         system("CLS");
 
@@ -76,10 +76,10 @@ namespace ui::test
         std::cout << "      Question: [<" << question_number << "> / <" << question_amount << ">]: " << question << std::endl;
         std::cout << "      Correct:    Marked:     Answers:" << std::endl;
 
-        for (const auto &answer: answers)
+        for (const auto& answer : answers)
         {
             bool is_marked = false;
-            for (const auto &correct_answer: correct_answers)
+            for (const auto& correct_answer : correct_answers)
             {
                 if (answer == correct_answer)
                 {
@@ -92,7 +92,7 @@ namespace ui::test
                 std::cout << "    [ ]";
 
             is_marked = false;
-            for (const auto &marked_answer: marked_answers)
+            for (const auto& marked_answer : marked_answers)
             {
                 if (answer == marked_answer)
                 {
@@ -108,9 +108,9 @@ namespace ui::test
         std::cout << "r - report, -> next, <- previous, q - quit";
     }
 
-    void show_take_question(const std::string &test_name, const std::string &question, const std::vector<std::string> &answers,
-                            const std::vector<std::string> &marked_answers, const std::string &selected_answer,
-                            int question_number, int question_amount)
+    void show_take_question(const std::string& test_name, const std::string& question, const std::vector<std::string>& answers,
+        const std::vector<std::string>& marked_answers, const std::string& selected_answer,
+        int question_number, int question_amount)
     {
         system("CLS");
 
@@ -124,34 +124,38 @@ namespace ui::test
         std::cout << "  Question: [<" << question_number << "> / <" << question_amount << ">]: " << question << std::endl;
         std::cout << "  Answers:" << std::endl;
 
-        for (const auto &answer_: answers)
+        bool is_marked = false;
+
+        for (const auto& answer_ : answers)
         {
-            bool is_marked = false;
-            for (auto &marked_answer: marked_answers)
+            if (answer_ == selected_answer && is_marked == false)
             {
-                if (answer_ == marked_answer)
+                std::cout << "  ";
+                ui::UI::set_console_text_background_color(3, 0);
+                std::cout << "-> ";
+                ui::UI::color_reset();
+            }
+
+            else
+                std::cout << "     ";
+
+            for (auto& marked_answer : marked_answers)
+            {
+                if (answer_ == marked_answer && is_marked == false)
                 {
-                    std::cout << "         [X] <";
+                    std::cout << "       [X]";
                     is_marked = true;
                     break;
                 }
             }
             if (!is_marked)
-                std::cout << "         [ ] <";
-
-            if (answer_ == selected_answer)
-            {
-                ui::UI::set_console_text_background_color(0, 3);
-                std::cout << answer_;
-                ui::UI::color_reset();
-                std::cout << std::endl;
-            }
+                std::cout << "         [ ]";
         }
         std::cout << std::endl;
         std::cout << "enter - mark/unmark answer, q - quit";
     }
 
-    void add_test(const std::string &entered_name)
+    void add_test(const std::string& entered_name)
     {
         system("CLS");
 
