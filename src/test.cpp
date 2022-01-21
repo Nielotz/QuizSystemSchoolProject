968b68d9dd5edec129ce820062df26dc5c9868b1
Nielotz <mati2806@poczta.fm>
2022-01-21 13:51:08 +0100

    Fix not saving reports

--- src/test.cpp
+++ src/test.cpp
@@ -14,7 +14,7 @@ void test::handle(const test_data::TestData &test_data, control::test_menu::Opti
             database::test::set_test(control::test::edit(test_data, username));
             break;
         case control::test_menu::OptionType::kReview:
-            control::test::review(test_data, username);
+            database::test::set_test(control::test::review(test_data, username));
             break;
         case control::test_menu::OptionType::kTake:
             database::test::set_test(control::test::take(test_data, username));
