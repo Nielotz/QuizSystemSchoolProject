40053ac4e2c2df7631ec9f723cfeb93f2fe41c02
Nielotz <mati2806@poczta.fm>
2022-02-01 22:59:07 +0100

    Add databse user unittest

---
+++ tests/database_user_unittest.cc
@@ -0,0 +1,35 @@
+#include "../src/database/user.cpp"
+#include "cassert"
+
+int main()
+{
+    database::user::path = "../database_user.txt";
+
+    // Test student.
+    {
+        UserData expected_user_data = {"student", "pass", AccountType::kStudent};
+        std::optional<UserData> received_user_data = database::user::get_user("student", "pass");
+        assert(received_user_data.has_value());
+        assert(expected_user_data.password == received_user_data->password);
+        assert(expected_user_data.username == received_user_data->username);
+        assert(expected_user_data.account_type == received_user_data->account_type);
+    }
+
+    // Test teacher.
+    {
+        UserData expected_user_data = {"teacher", "pass", AccountType::kTeacher};
+        std::optional<UserData> received_user_data = database::user::get_user("teacher", "pass");
+        assert(received_user_data.has_value());
+        assert(expected_user_data.password == received_user_data->password);
+        assert(expected_user_data.username == received_user_data->username);
+        assert(expected_user_data.account_type == received_user_data->account_type);
+    }
+
+    // Test invalid.
+    {
+        std::optional<UserData> received_user_data = database::user::get_user("not_existing", "pass");
+        assert(!received_user_data.has_value());
+    }
+
+    return 0;
+}
